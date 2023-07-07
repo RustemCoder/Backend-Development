3ce121301ac9e68086099a9de4fa24932fe18aeb
Rustem <48765033+RustemCoder@users.noreply.github.com>
2023-07-07 09:18:20 -0400

    Update createServer.js

--- http/createServer.js
+++ http/createServer.js
@@ -1,3 +1,6 @@
+//http package is used for establishing remote connections to a server or to create a server which listens to client.
+//createServer - Takes a requestListener, a function which takes request and response parameters, where request is the handle to the request from the client and response is the handle to be sent to the client.
+
 const http = require('http');
 const requestListener = function(req, res) {
   res.writeHead(200);
