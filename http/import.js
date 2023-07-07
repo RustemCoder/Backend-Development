cd71e8bde80b5896d373d05715fa87cf0e818a67
Rustem <48765033+RustemCoder@users.noreply.github.com>
2023-07-07 09:21:41 -0400

    Update import.js

--- http/import.js
+++ http/import.js
@@ -1,3 +1,5 @@
+//The import statement is used to import modules that some other module has exported. A file that includes reusable code is known as a module.
+
 // addTwoNos.mjs
 function addTwo(num) {
   return num + 4;
